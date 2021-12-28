c39b822ad72ab91ce43067c2b057359129f35d0a
Adam Ciesielski <ciesielskiadam6@gmail.com>
2021-12-28 15:23:54 +0100

    add basic login page, change Header styles

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './layout/Header/Header';
 
 import './App.scss';
+import LoginPage from './pages/LoginPage/LoginPage';
 
 function App() {
   return (
     <div className='app'>
       <Header />
+      <LoginPage />
     </div>
   );
 }
