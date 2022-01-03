4f7f298351195dc22abe6482434abba508f4e909
Adam Ciesielski <ciesielskiadam6@gmail.com>
2022-01-03 23:20:11 +0100

    split Header component into Header and MainNav components

---
+++ client/src/components/MainNav/MainNav.jsx
@@ -0,0 +1,37 @@
+import { Link } from 'react-router-dom';
+
+const MainNav = ({ isMenuOpen }) => {
+  return (
+    <nav className={isMenuOpen ? 'nav nav--active' : 'nav'}>
+      <ul className='nav__list'>
+        <li className='nav__element'>
+          <Link className='nav__link' to='/category/dogfood'>
+            Dog Food
+          </Link>
+        </li>
+        <li className='nav__element'>
+          <Link className='nav__link' to='/category/catfood'>
+            Cat Food
+          </Link>
+        </li>
+        <li className='nav__element'>
+          <Link className='nav__link' to='/category/toys'>
+            Toys
+          </Link>
+        </li>
+        <li className='nav__element'>
+          <Link className='nav__link' to='/category/other'>
+            Other
+          </Link>
+        </li>
+        <li className='nav__element'>
+          <Link className='nav__link' to='/auth'>
+            My Account
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default MainNav;
--- client/src/layout/Header/Header.jsx
+++ client/src/layout/Header/Header.jsx
@@ -1,8 +1,10 @@
 import './Header.scss';
+
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars, faTimes, faSearch } from '@fortawesome/free-solid-svg-icons';
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import MainNav from '../../components/MainNav/MainNav';
 
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -73,35 +75,7 @@ function Header() {
           <input className='search__input' type='text' />
           <button className='search__submit'>Search</button>
         </div>
-        <nav className={isMenuOpen ? 'nav nav--active' : 'nav'}>
-          <ul className='nav__list'>
-            <li className='nav__element'>
-              <Link className='nav__link' to='/category/dogfood'>
-                Dog Food
-              </Link>
-            </li>
-            <li className='nav__element'>
-              <Link className='nav__link' to='/category/catfood'>
-                Cat Food
-              </Link>
-            </li>
-            <li className='nav__element'>
-              <Link className='nav__link' to='/category/toys'>
-                Toys
-              </Link>
-            </li>
-            <li className='nav__element'>
-              <Link className='nav__link' to='/category/other'>
-                Other
-              </Link>
-            </li>
-            <li className='nav__element'>
-              <Link className='nav__link' to='/auth'>
-                My Account
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <MainNav isMenuOpen={isMenuOpen} />
       </header>
     </>
   );
