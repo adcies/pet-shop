49aa89beee3a11c85c9a2bbc18667cb858891592
Adam Ciesielski <ciesielskiadam6@gmail.com>
2022-02-23 11:23:58 +0100

    add products to database, style product card

--- client/src/components/Product/Product.jsx
+++ client/src/components/Product/Product.jsx
@@ -1,4 +1,5 @@
 import './Product.scss';
+import { Link } from 'react-router-dom';
 
 const Product = ({ product }) => {
   const { name, description, price, img } = product;
@@ -11,7 +12,12 @@ const Product = ({ product }) => {
     <div className='product'>
       <p className='product__name'>{name}</p>
       <p className='product__description'>{description}</p>
-      <p className='product__price'>{price}</p>
+      <p className='product__price'> Price: {price} zł</p>
+      <div className='product__buy'>
+        <Link className='product__buy-btn' to='/auth'>
+          Buy now
+        </Link>
+      </div>
       <div style={imageStyle} className='product__img'></div>
     </div>
   );
