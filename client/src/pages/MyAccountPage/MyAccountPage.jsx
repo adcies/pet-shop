50c69777b44b9c1cc865cd6de501ac08a7fe8460
Adam Ciesielski <ciesielskiadam6@gmail.com>
2022-03-05 08:46:13 +0100

    add basic info to My Account page

--- client/src/pages/MyAccountPage/MyAccountPage.jsx
+++ client/src/pages/MyAccountPage/MyAccountPage.jsx
@@ -1,7 +1,28 @@
 import './MyAccountPage.scss';
+import { useSelector } from 'react-redux';
 
 const MyAccountPage = () => {
-  return <div>My Account</div>;
+  const {
+    user: { purchasedProducts, login, wallet },
+  } = useSelector(state => state.user);
+
+  return (
+    <section className='my-account'>
+      <p className='my-account__hello'>Hello, {login}</p>
+      <p className='my-account__wallet'>
+        Your wallet:
+        <span className='my-account__wallet-money'>{wallet}</span>
+      </p>
+      <div className='purchased-products'>
+        Purchased products:
+        {purchasedProducts.map(product => (
+          <div key={product.id} className='purchased-product__item'>
+            <p className='purchased-product__name'>{product.name}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default MyAccountPage;
